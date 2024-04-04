f956b262ca3f3d355a0076e4b83271d3b41909b9
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-04-04 17:46:47 -0500

    Changing HomeUser

--- frontend/src/components/profileUser/HomeUser.tsx
+++ frontend/src/components/profileUser/HomeUser.tsx
@@ -1,15 +1,23 @@
 import { NavLink } from 'react-router-dom'
-import styles from './HomeAdmin.module.css'
+import styles from './HomeUser.module.css'
+import UserForm from '../userForm/UserForm'
 
-const HomeAdmin = () => {
+const HomeUser = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Mi perfil</h2>
-      <NavLink to='/editarperfil' className={styles.options}>Editar perfil</NavLink>
-      <NavLink to='/mispedidos' className={styles.options}>Mis pedidos</NavLink>
-      <NavLink to='/' className={styles.options}>Cerrar sesión</NavLink>
+      <span>
+        <div className={styles.containerMenu}>
+          <h2 className={styles.title}>Mi perfil</h2>
+          <NavLink to='/useraccount' className={styles.options}>Editar perfil</NavLink>
+          <NavLink to='/useraccount/mispedidos' className={styles.options}>Mis pedidos</NavLink>
+          <NavLink to='/' className={styles.close}>Cerrar sesión</NavLink>
+        </div>
+      </span>
+      <span className={styles.profile}>
+        <UserForm  />
+      </span>
     </div>
   )
 }
 
-export default HomeAdmin
+export default HomeUser
