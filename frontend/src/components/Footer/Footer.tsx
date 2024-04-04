b97293a907d200f26227c8440152f921d2c3c651
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-04-04 16:02:22 -0300

    cambios footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,44 +1,17 @@
-// import Style from './Footer.module.css'
-
-// const Footer = () => {
-//   return(
-//     <footer className={Style.footer}>
-//       <div className={Style.conteiner}>
-//         <h3 className={Style.title}>INTERFOODS!</h3>
-//         <div className={Style.conteinerList}>
-//           <h3 className={Style.links}>Nuestros platos</h3>
-//           <h3 className={Style.links}>Como funciona</h3>
-//           <h3 className={Style.links}>¿Quienes somos?</h3>
-//           <h3 className={Style.links}>FAQ'S</h3>
-//         </div>
-//         <div className={Style.conteinerList}>
-//           <h3 className={Style.links}> Mis compras</h3>
-//         </div>
-//       </div>
-//     </footer>
-//   )
-// }
-
-// export default Footer
-
-import React from 'react';
-import { Link } from 'react-router-dom'; // Importa Link de React Router
+import { Link, NavLink } from 'react-router-dom'; // Importa Link de React Router
 import styles from './Footer.module.css'; // Usa estilos directamente
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <h3 className={styles.title}><Link to="/">INTERFOODS!</Link></h3>
+        <h3 className={styles.title}><NavLink className={styles.title} to="/">INTERFOODS!</NavLink></h3>
         <div className={styles.containerList}>
           <hr></hr>
-          <h3 className={styles.links}><Link to="/NuestrosPlatos">Nuestros platos</Link></h3>
-          <h3 className={styles.links}><Link to="/Comofunciona">Cómo funciona</Link></h3>
-          <h3 className={styles.links}><Link to="/QuienesSomos">¿Quiénes somos?</Link></h3>
-          <h3 className={styles.links}><Link to="/Faqs">FAQ'S</Link></h3>
-        </div>
-        <div className={styles.containerList}>
-          <h3 className={styles.links}><Link to="/cart">Mis compras</Link></h3>
+          <h3 className={styles.links}><NavLink className={styles.links} to="/NuestrosPlatos">Nuestros platos</NavLink></h3>
+          <h3 className={styles.links}><NavLink className={styles.links} to="/Comofunciona">Cómo funciona</NavLink></h3>
+          <h3 className={styles.links}><NavLink className={styles.links} to="/QuienesSomos">¿Quiénes somos?</NavLink></h3>
+          <h3 className={styles.links}><NavLink className={styles.links} to="/Faqs">FAQ'S</NavLink></h3>
         </div>
       </div>
     </footer>
