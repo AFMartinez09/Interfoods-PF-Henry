3a446d13cfdda75ae2359c931c9db4575b40992e
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-04-04 16:03:09 -0300

    INTERFOODS

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,11 +1,11 @@
-import { Link, NavLink } from 'react-router-dom'; // Importa Link de React Router
+import { NavLink } from 'react-router-dom'; // Importa Link de React Router
 import styles from './Footer.module.css'; // Usa estilos directamente
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <h3 className={styles.title}><NavLink className={styles.title} to="/">INTERFOODS!</NavLink></h3>
+        <h3 className={styles.title}><NavLink className={styles.title} to="/">INTERFOODS</NavLink></h3>
         <div className={styles.containerList}>
           <hr></hr>
           <h3 className={styles.links}><NavLink className={styles.links} to="/NuestrosPlatos">Nuestros platos</NavLink></h3>
