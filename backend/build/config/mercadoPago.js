fe6391f42a59c82e3098bd9407bac3c2b7b72da1
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-04-08 13:27:04 -0300

    tsc dotenv

--- backend/build/config/mercadoPago.js
+++ backend/build/config/mercadoPago.js
@@ -1,12 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pagos = exports.preference = void 0;
 const mercadopago_1 = require("mercadopago");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const accessToken = process.env.TOKEN_MERCADO_PAGO || "";
 const client = new mercadopago_1.MercadoPagoConfig({
     accessToken: accessToken,
--- backend/build/config/nodemailer.js
+++ backend/build/config/nodemailer.js
@@ -4,8 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.transporter = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const nodemailer_1 = __importDefault(require("nodemailer"));
 exports.transporter = nodemailer_1.default.createTransport({
     service: "hotmail",
