72319061e80260505fbabc2321c1c11e114f3df3
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-04-12 01:31:08 -0300

    getUserById y reseña con mail del creador

--- backend/src/routes/registerRouter.ts
+++ backend/src/routes/registerRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { signUpNewUser } from "../handlers/signUpNewUser";
 import { newUser } from "../controllers/newUserDb";
-import { findUserByEmail } from "../controllers/getUserByName";
+import { findUserByEmail } from "../controllers/getUserByEmail";
 import { updateUser } from "../controllers/putUser";
 import { getAllUsers } from "../controllers/getAllUsers";
+import {findUserById} from "../controllers/getUserById"
 const router = express.Router();
 
 router.post("/signup", signUpNewUser);
@@ -11,5 +12,6 @@ router.post("/signupDb", newUser);
 router.get('/usuario/:email', findUserByEmail)
 router.get("/usuarios", getAllUsers)
 router.put('/usuario/update/:email', updateUser)
+router.get('/getUsuario/:id', findUserById)
 
 export default router;
