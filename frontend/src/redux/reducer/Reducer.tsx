c6545a5e1a340d478b0afc1c9dd0e2c4225296b8
Mirko1523 <134454433+Mirko1523@users.noreply.github.com>
2024-04-13 17:42:00 -0300

    Fix Stock Graphics with real value

--- frontend/src/redux/reducer/Reducer.tsx
+++ frontend/src/redux/reducer/Reducer.tsx
@@ -14,7 +14,7 @@ import {
 
 
 
-interface Plato {
+export interface Plato {
   id: number;
   nombre: string;
   origen: string;
@@ -164,3 +164,4 @@ const Reducer = (state: StoreState = initialState, action: Action): StoreState =
 }
 
 export default Reducer;
+
