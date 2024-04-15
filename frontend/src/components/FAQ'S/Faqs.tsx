f3b3aec301f1fe01cb18e7ba2bae3492fa39817d
Gaston Vergagni <116928445+Gastonvg@users.noreply.github.com>
2024-04-15 17:07:07 -0300

    arreglos de quienes somos, como funciona, faqs
    arreglos de quienes somos, como funciona, faqs

--- frontend/src/components/FAQ'S/Faqs.tsx
+++ frontend/src/components/FAQ'S/Faqs.tsx
@@ -8,19 +8,19 @@ const Faqs: React.FC<FaqsProps> = () => {
   const [preguntas] = useState([
     {
       pregunta: "¿Qué es InterFoods?",
-      respuesta: "InterFoods es un servicio de entrega de comida a domicilio que te permite descubrir y disfrutar de la mejor gastronomía internacional."
+      respuesta: "InterFoods es una plataforma líder en el mercado de servicios de entrega de comida a domicilio, dedicada a ofrecerte una experiencia culinaria única y diversa desde la comodidad de tu hogar. Nos enorgullece proporcionar a nuestros usuarios acceso a una amplia gama de deliciosas opciones gastronómicas de todo el mundo, permitiéndote descubrir y disfrutar de la mejor cocina internacional sin tener que salir de casa."
     },
     {
       pregunta: "¿Cómo funciona el servicio?",
-      respuesta: "Es muy sencillo. Solo tienes que elegir tu ciudad, seleccionar los platos que más te gusten y nosotros te los llevamos a casa."
+      respuesta: "Nuestro servicio es increíblemente sencillo de usar. Todo lo que necesitas hacer es iniciar sesión en nuestra plataforma, donde te recibirán una gran variedad de opciones culinarias de diferentes partes del mundo."
     },
     {
       pregunta: "¿Dónde y cuándo entregan?",
-      respuesta: "Entregamos en toda la ciudad en la cual estes, de lunes a domingo, de 12:00 a 23:00 horas."
+      respuesta: "Entregamos en qualquier parte de latinoamerica. de lunes a domingo, de 12:00 a 23:00 horas."
     },
     {
       pregunta: "¿Hay un importe mínimo para realizar un pedido?",
-      respuesta: "No,no hay ningun importe mínimo!, solo elige el plato o los platos que quieras y te lo enviamos! ."
+      respuesta: "En InterFoods, creemos en la libertad de elección y en hacer que disfrutes de la mejor comida sin restricciones. Es por eso que no imponemos ningún importe mínimo en tus pedidos. Ya sea que desees disfrutar de un solo plato o de una variedad de delicias, ¡nuestro servicio está disponible para ti!"
     },
     {
       pregunta: "¿Puedo realizar un pedido el día que quiera?",
@@ -32,7 +32,7 @@ const Faqs: React.FC<FaqsProps> = () => {
     },
     {
       pregunta: "¿Se puede realizar un pedido por teléfono o WhatsApp?",
-      respuesta: "Sí, puedes realizar un pedido por teléfono al 3446-309499 o por WhatsApp al 3446-309499."
+      respuesta: "Sí, puedes realizar un pedido por teléfono al 3446-309499 o por WhatsApp al 3446-309499. Tambien puedes dirigirte a nuestro apartado de contactos para mas opciones"
     },
     {
       pregunta: "¿Qué pasa si mi pedido llega golpeado o en mal estado?",
@@ -40,7 +40,7 @@ const Faqs: React.FC<FaqsProps> = () => {
     },
  {
       pregunta: "¿Se puede devolver un pedido si no me gusta?",
-      respuesta: "No hay opción de devolver un pedido, pero si por cualquier cosa no ha cumplido tus expectativas, ponte en contacto con nosotros para conocer más en detalle el caso y poder ofrecerte una solución."  
+      respuesta: "No, no hay opción de devolver un pedido, pero si por cualquier cosa no ha cumplido tus expectativas, ponte en contacto con nosotros para conocer más en detalle el caso y poder ofrecerte una solución."  
     },
 
   ]);
