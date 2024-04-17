6c029ba4dd879a1ec47fdd3b6c746c9b0760ebd4
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-04-17 19:32:11 -0300

    home acomodado

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -13,8 +13,8 @@ const Home = () => {
         <div>
           <h3 className={Style.title}>Come rico toda la semana</h3>
           <ul className={Style.lista}>
-            <li className={Style.li}>
-              <svg
+            <div className={Style.lisCon}>
+            <svg
                 className={Style.svg}
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -23,11 +23,12 @@ const Home = () => {
               >
                 <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z" />
               </svg>
+            <li className={Style.li}>
               Recibe donde y cuando quieras.
             </li>
-            <li className={Style.li}>
-              {" "}
-              <svg
+            </div>
+            <div className={Style.lisCon}>
+            <svg
                 className={Style.svg}
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -36,11 +37,12 @@ const Home = () => {
               >
                 <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z" />
               </svg>
+            <li className={Style.li}>
               Cancela en cualquier momento.
             </li>
-            <li className={Style.li}>
-              {" "}
-              <svg
+            </div>
+            <div className={Style.lisCon}>
+            <svg
                 className={Style.svg}
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -49,11 +51,12 @@ const Home = () => {
               >
                 <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z" />
               </svg>
+            <li className={Style.li}>
               ¡Comida de toda latinoamerica!
             </li>
-            <li className={Style.li}>
-              {" "}
-              <svg
+            </div>
+            <div className={Style.lisCon}>
+            <svg
                 className={Style.svg}
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -62,8 +65,10 @@ const Home = () => {
               >
                 <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z" />
               </svg>
+            <li className={Style.li}>
               Sin cocinar, sin limpiar… ¡Listo en minutos!
             </li>
+            </div>
           </ul>
         </div>
         <img
